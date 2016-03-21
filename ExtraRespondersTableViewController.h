7ef0f3f9ba875e30457e0c6be5652bd7fda7ca48
danielllewellyn <danieln.llewellyn@gmail.com>
2016-03-21 10:06:50 +0000

    Ripples

---
+++ ExtraRespondersTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExtraRespondersTableViewController.h
+//  Ripple
+//
+//  Created by Daniel Llewellyn on 20/03/2016.
+//  Copyright © 2016 Daniel Llewellyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExtraRespondersTableViewController : UITableViewController
+
+@end
